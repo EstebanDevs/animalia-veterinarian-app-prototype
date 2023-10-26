dec3f674ff0f07e367d7983aefe080f38b946d12
Esteban Perafan <bestebanperafan@gmail.com>
2023-10-26 01:51:32 -0300

    Add authentication functionality and switch the previous users model to sessions model

--- controllers/authController.php
+++ controllers/authController.php
@@ -1,16 +1,48 @@
 <?php
-    require '../models/user.php';
+    require '../models/sessions.php';
+    //Iniciar sesión para credenciales
+    session_start();
 
-try {
-    //Encontrar al usuario
-    $user = find_user($email, $password);
-    
-    //Acreditar que si el usuario existe en la base de datos
-    if(!$user){
-        include '../index.php';
-    } else {
-        include '../views/pets.php';
+    try {
+        //Validar que se recibio una petición POST en el servidor
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            //Obtener las credenciales enviadas por el metodo POST
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            //Buscar la sesión en la bd
+            $session = getSession($email, $password);
+            
+            //Acreditar que el usuario existe en la base de datos para brindarle acceso
+            if(!$session){
+                //Reenviar de nuevo al login
+                header('Location: ../index.php');
+                exit;
+                //Si existe la sesión
+            } else {
+                //Volver accesibles los datos de la consulta
+                $session = $session->fetch_assoc();
+                
+                //Validar si el usuario es persona particular o veterinario
+                if($session['User_RUT_FK'] != null){
+                    //Es persona particular
+                    //Guardar la sesión para realizar peticiones a la bd con el usuario loggeado
+                    $_SESSION['Rol'] = 'User';
+                    $_SESSION['RUT'] = $session['User_RUT_FK'];
+                    $_SESSION['Email'] = $session['Email'];
+                    $_SESSION['Password'] = $session['Password'];
+                    header("Location: ../views/pets.php");
+                    exit;
+                } else {
+                    //Es veterinario, como por ahora no tiene acceso solo se le devuelve al login
+                    header("Location: ../index.php");
+                    exit;
+                }
+            }
+        }
+
+    } catch(\Throwable $error) {
+        var_dump($error);
     }
-} catch(\Throwable $error) {
-    var_dump($error);
-}
+
+?>
---
+++ models/sessions.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function getSession($email, $password)
+{
+    try{
+        //Obtener conexión a la base de datos
+        require '../config/dbConnection.php'; 
+
+        //Consulta SQL
+        $sql = "SELECT * FROM sessions WHERE Email = '$email' AND Password = '$password';";
+
+        //Ejecutar SQL
+        $query = mysqli_query($conn, $sql);
+        
+        return $query;
+    } catch (\Throwable $error) {
+        var_dump($error);
+    }
+}
+
+
+?>
