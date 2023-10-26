04a64101328e3a046822857f01ed2b04c4b4ae18
FrendCodes <rosangelarispeg@gmail.com>
2023-10-25 22:47:46 -0300

    initial wireframes and components for the fronn

---
+++ views/petCard.php
@@ -0,0 +1,15 @@
+<div class="card">
+    <img src="resources/testpaw.png" alt="foto" class="card-img">
+    <div class="card-details">
+        <h1 class="card-details_name">
+            Pepito
+        </h1>
+        <p class="card-details_description">
+            Descripcion de Pepito, el perro o gato, lo que
+        </p>
+    </div>
+    <aside class="card-options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </aside>   
+</div>
---
+++ views/templates/header.php
@@ -0,0 +1,10 @@
+<header>
+    <img src="testpaw.png" alt="foto" class="header_logo">
+
+    <nav class="header_navegation">
+        <span class="nav-element">Contacto</span>
+        <span class="nav-element">Nosotros</span>
+        <span class="nav-element">Ayuda</span>
+    </nav>
+
+</header>
