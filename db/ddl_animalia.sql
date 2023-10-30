6985d6e85fd14bb73558d371c140c4635925a170
FrendCodes <rosangelarispeg@gmail.com>
2023-10-30 03:15:08 -0300

    change in db (ON DELETE CASCADE) to make deleting process smoother

--- db/ddl_animalia.sql
+++ db/ddl_animalia.sql
@@ -8,12 +8,12 @@ CREATE TABLE species(
     primary key(Specie_ID)
 );
 
-CREATE TABLE races(
-    Race_ID int(10) auto_increment not null,
-    Race varchar(50) not null,
+CREATE TABLE breeds(
+    Breed_ID int(10) auto_increment not null,
+    Breed varchar(50) not null,
     Specie_ID_FK int(10) not null,
-    primary key(Race_ID),
-    constraint races_species_FK 
+    primary key(breed_ID),
+    constraint breed_species_FK 
     foreign key(Specie_ID_FK) references species(Specie_ID)
 );
 
@@ -35,64 +35,30 @@ CREATE TABLE status(
     primary key(Status_ID)
 );
 
+CREATE TABLE roles(
+    Rol_ID int(10) auto_increment not null,
+    Name varchar(80) not null,
+    primary key(Rol_ID)
+);
+
+CREATE TABLE user_accounts(
+    Account_ID int(10) auto_increment not null,
+    Email varchar(255) not null,
+    Password varchar(80) not null,
+    Rol_ID_FK int(10) not null,
+    primary key(Account_ID),
+    constraint user_rol_fk 
+    foreign key(Rol_ID_FK) references roles(Rol_ID)
+);
+
 CREATE TABLE vets(
     RUT int(9) not null,
     Name varchar(30) not null,
     Surname varchar(30) not null,
-    primary key(RUT)
-);
-
-
-CREATE TABLE pets(
-    Pet_ID int(10) auto_increment not null,
-    Name varchar(30) not null,
-    Genre varchar(30) not null,
-    Age int(3) not null,
-    Race_ID_FK int(10) not null,
-    primary key(Pet_ID),
-    constraint pet_race_fk 
-    foreign key(Race_ID_FK) references races(Race_ID)
-);
-
-CREATE TABLE users(
-    RUT int(9) not null,
-    Name varchar(30) not null,
-    Surname varchar(30),
-    primary key(RUT)
-);
-
-CREATE TABLE users_pets(
-    Users_pets_ID int(10) auto_increment not null,
-    User_RUT_FK int(9) not null,
-    Pet_ID_FK int(10) not null,
-    Creation_date datetime not null default current_timestamp,
-    primary key(Users_pets_ID),
-    constraint user_RUT_pets_FK 
-    foreign key(User_RUT_FK) references users(RUT),
-    constraint pet_ID_user_FK
-    foreign key(Pet_ID_FK) references pets(Pet_ID)
-);
-
-CREATE TABLE users_addresses(
-    User_address_ID int(10) auto_increment not null,
-    User_RUT_FK int(9) not null,
-    Address_ID_FK int(10) not null,
-    primary key(User_address_ID),
-    constraint users_address_FK
-    foreign key(User_RUT_FK) references users(RUT),
-    constraint address_user_FK  
-    foreign key(Address_ID_FK) references addresses(Address_ID)
-);
-
-CREATE TABLE users_phones(
-    User_phones_ID int(10) auto_increment not null,
-    User_RUT_FK int(9) not null,
-    Phone_ID_FK int(9) not null,
-    primary key(User_phones_ID),
-    constraint users_phones_FK
-    foreign key(User_RUT_FK) references users(RUT),
-    constraint phone_user_FK  
-    foreign key(Phone_ID_FK) references phones(Phone_ID)
+    Vet_account_ID_FK int(10) not null,
+    primary key(RUT),
+    constraint vets_account_fk
+    foreign key(Vet_account_ID_FK) references user_accounts(Account_ID)
 );
 
 CREATE TABLE vets_phones(
@@ -117,6 +83,62 @@ CREATE TABLE vets_addresses(
     foreign key(Address_ID_FK) references addresses(Address_ID)
 );
 
+CREATE TABLE pets(
+    Pet_ID int(10) auto_increment not null,
+    Name varchar(30) not null,
+    Sex varchar(30) not null,
+    Age int(3) not null,
+    Breed_ID_FK int(10) not null,
+    primary key(Pet_ID),
+    constraint pet_race_fk 
+    foreign key(Breed_ID_FK) references breeds(Breed_ID)
+);
+
+CREATE TABLE users(
+    RUT int(9) not null,
+    Name varchar(30) not null,
+    Surname varchar(30),
+    User_account_ID_FK int(10) not null,
+    primary key(RUT),
+    constraint users_account_fk
+    foreign key(User_account_ID_FK) references user_accounts(Account_ID)
+);
+
+CREATE TABLE users_pets(
+    Users_pets_ID int(10) auto_increment not null,
+    User_RUT_FK int(9) not null,
+    Pet_ID_FK int(10) not null,
+    Creation_date datetime not null default current_timestamp,
+    primary key(Users_pets_ID),
+    constraint user_RUT_pets_FK 
+    foreign key(User_RUT_FK) references users(RUT),
+    constraint pet_ID_user_FK
+    foreign key(Pet_ID_FK) references pets(Pet_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE users_addresses(
+    User_address_ID int(10) auto_increment not null,
+    User_RUT_FK int(9) not null,
+    Address_ID_FK int(10) not null,
+    primary key(User_address_ID),
+    constraint users_address_FK
+    foreign key(User_RUT_FK) references users(RUT),
+    constraint address_user_FK  
+    foreign key(Address_ID_FK) references addresses(Address_ID)
+);
+
+CREATE TABLE users_phones(
+    User_phones_ID int(10) auto_increment not null,
+    User_RUT_FK int(9) not null,
+    Phone_ID_FK int(9) not null,
+    primary key(User_phones_ID),
+    constraint users_phones_FK
+    foreign key(User_RUT_FK) references users(RUT),
+    constraint phone_user_FK  
+    foreign key(Phone_ID_FK) references phones(Phone_ID)
+);
+
+
 CREATE TABLE attention_records(
     Attention_record_ID int(10) auto_increment not null,
     Pet_ID_FK int(10) not null,
@@ -126,25 +148,12 @@ CREATE TABLE attention_records(
     Treatment varchar(255),
     Note text,
     Visit_date date not null,
-    Creation_date datetime default current_timestamp,
+    Creation_date datetime not null default current_timestamp,
     primary key(Attention_record_ID),
     constraint pet_attention_records_FK 
-    foreign key(Pet_ID_FK) references pets(Pet_ID),
-    constraint vet_attention_records_FK
+    foreign key(Pet_ID_FK) references pets(Pet_ID) ON DELETE CASCADE,
+    constraint vets_attention_records_FK
     foreign key(Vet_RUT_FK) references vets(RUT),
     constraint status_attention_records_FK
     foreign key(Status_ID_FK) references status(Status_ID)
-);
-
-CREATE TABLE sessions(
-    Session_ID int(10) auto_increment not null,
-    Email varchar(255) not null,
-    Password varchar(80) not null,
-    User_RUT_FK int(9),
-    Vet_RUT_FK int(10),
-    primary key(Session_ID),
-    constraint user_session_FK
-    foreign key(User_RUT_FK) references users(RUT), 
-    constraint vet_session_FK
-    foreign key(Vet_RUT_FK) references vets(RUT)
 );
