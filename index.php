874b80ab66596c5373d7fe2bf66663a3067f212d
Esteban Perafan <bestebanperafan@gmail.com>
2023-10-30 20:49:03 -0300

    Add login styles

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Iniciar sesión | Animalia</title>
-    </head>
-    <body>
-        <form action="./controllers/authController.php" method="post">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
+<head>
+  <link rel="stylesheet" type="text/css" href="./views/styles/styles.css">
+</head>
+<body>
+  <div id="login-container">
+    <div id="logo">
+      <img src="./views/resources/pata-de-perro.png" alt="Logo de Animalia">
+    </div>
+    <form class="login-form" action="./controllers/authController.php" method="post">
+      <label for="username">Correo:</label>
+      <div class="input-container">
+        <input type="text" id="email" name="email" required>
+      </div>
 
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
+      <label for="password">Contraseña:</label>
+      <div class="input-container">
+        <input type="password" id="password" name="password" required>
+      </div>
 
-            <button>Enviar</button>
-        </form>
-    </body>
+      <input type="submit" value="Iniciar sesión" class="login-button">
+    </form>
+  </div>
+</body>
 </html>
